4d87a1c4662dec8beecd7f30dfb28fcb7db142b2
Adri <miri.unza0192@gmail.com>
2025-06-06 19:53:19 -0400

    agregando app historiales

--- grupo7Proy/historiales/apps.py
+++ grupo7Proy/historiales/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdopcionesConfig(AppConfig):
+class HistorialesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adopciones'
+    name = 'historiales'
