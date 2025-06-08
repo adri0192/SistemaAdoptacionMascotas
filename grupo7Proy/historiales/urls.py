64ebc342511b16f5cddabc045a047f0da979793a
Angelica <angelicaespinoza203@gmail.com>
2025-06-08 19:08:19 -0400

    actualizando

--- grupo7Proy/historiales/urls.py
+++ grupo7Proy/historiales/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mascota/<int:mascota_id>/historial/', views.historial_lista, name='historial_lista'),
-    path('mascota/<int:mascota_id>/historial/nuevo/', views.historial_nuevo, name='historial_nuevo'),
+    path('historial/<int:mascota_id>', views.historial_lista, name='historial_lista'),
+    path('historialNuevo/<int:mascota_id>', views.historial_nuevo, name='historial_nuevo'),
 ]
