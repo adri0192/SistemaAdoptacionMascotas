22a9e307931de8efece7a11b23c08c8decba1623
Angelica <angelicaespinoza203@gmail.com>
2025-06-08 17:47:10 -0400

    actualizando modelos

--- grupo7Proy/historiales/models.py
+++ grupo7Proy/historiales/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.db import models
+from mascotas.models import Mascota
 
-class Mascota(models.Model):
-    nombre = models.CharField(max_length=100)
 
 
 class HistorialMedico(models.Model):
