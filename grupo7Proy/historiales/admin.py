a3c2f004547869772c8e860654b4cd9cec2773f7
Angelica <angelicaespinoza203@gmail.com>
2025-06-08 17:22:52 -0400

    registrando el modelo

--- grupo7Proy/historiales/admin.py
+++ grupo7Proy/historiales/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Mascota, HistorialMedico
+from .models import HistorialMedico
 
-admin.site.register(Mascota)
+
 admin.site.register(HistorialMedico)
