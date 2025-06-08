bbc40756318e3a6731c3782ca5807fe4be3d16b5
Angelica <angelicaespinoza203@gmail.com>
2025-06-08 15:49:24 -0400

    aplicando las migraciones de HistorilaMedico

--- grupo7Proy/historiales/models.py
+++ grupo7Proy/historiales/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+
+
+class HistorialMedico(models.Model):
+    mascota = models.ForeignKey(Mascota, on_delete=models.CASCADE, related_name='historiales')
+    fecha = models.DateField()
+    descripcion = models.TextField()
+    tratamiento = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Historial de {self.mascota.nombre} -Fecha: {self.fecha}"
+
+
+
--- grupo7Proy/historiales/views.py
+++ grupo7Proy/historiales/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Mascota, HistorialMedico
+from .forms import HistorialMedicoForm
 
-# Create your views here.
+def historial_lista(request, mascota_id):
+    mascota = get_object_or_404(Mascota, id=mascota_id)
+    historiales = mascota.historiales.all()
+    return render(request, 'historial_lista.html', {'mascota': mascota, 'historiales': historiales})
+
+def historial_nuevo(request, mascota_id):
+    mascota = get_object_or_404(Mascota, id=mascota_id)
+    if request.method == 'POST':
+        form = HistorialMedicoForm(request.POST)
+        if form.is_valid():
+            historial = form.save(commit=False)
+            historial.mascota = mascota
+            historial.save()
+            return redirect('historial_lista', mascota_id=mascota.id)
+    else:
+        form = HistorialMedicoForm()
+    return render(request, 'historial_form.html', {'form': form, 'mascota': mascota})
+
